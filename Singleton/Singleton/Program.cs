dcad5cf073684c85b8c16daed1c08b2671a522be
leonardieuris <gino.leonardi@euris.it>
2023-04-27 12:45:48 +0200

    firstcommit

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,18 @@
+﻿using SingletonLibrary;
+
+
+var myPrinter = PrintSpooler.GetPrintSpooler();
+
+myPrinter.Enqueue("ciao");
+
+var myPrinter2 = PrintSpooler.GetPrintSpooler();
+
+myPrinter2.Enqueue("bye");
+
+var myPrinter3 = PrintSpooler.GetPrintSpooler();
+
+
+myPrinter3.PrintALL();
+
+Console.WriteLine("fine");
+
---
+++ Singleton/SingletonLibrary/PrintSpooler.cs
@@ -0,0 +1,36 @@
+﻿namespace SingletonLibrary
+{
+    public sealed class PrintSpooler
+    {
+        private static PrintSpooler Instance;
+        private Queue<string> queueFiles;
+
+        private PrintSpooler()
+        {
+            queueFiles = new Queue<string>();
+        }
+
+
+        public static PrintSpooler GetPrintSpooler()
+        {
+            if(Instance == null)
+                Instance = new PrintSpooler();
+            return Instance;
+        }
+
+        public void Enqueue(string files)
+        {
+            queueFiles.Enqueue(files);
+        }
+
+
+        public void PrintALL()
+        {
+            foreach(string file in queueFiles) 
+             Console.WriteLine(file); 
+        }
+
+
+
+    }
+}
